9e8fd1b4ea71dbad418f812744bb9146a9ff261e
Dmitry Kotik <7944694+dkotik@users.noreply.github.com>
2024-11-27 19:41:41 +0200

    add minimal interactivity

--- cmd/wmcserver/index.go
+++ cmd/wmcserver/index.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	_ "embed" // for bundling index.html
+	"fmt"
 	"io"
 	"net/http"
 	"strings"
+	"time"
+
+	datastar "github.com/starfederation/datastar/code/go/sdk"
 )
 
 //go:embed index.html
@@ -13,4 +17,26 @@ var indexTemplate string
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	_, _ = io.Copy(w, strings.NewReader(indexTemplate))
+}
+
+func EventHandler(w http.ResponseWriter, r *http.Request) {
+	sse := datastar.NewSSE(w, r)
+
+	// Merges HTML fragments into the DOM.
+	sse.MergeFragments(
+		`<div id="question">What do you put in a toaster?</div>`)
+
+	// Merges signals into the store.
+	sse.MergeSignals([]byte(`{response: '', answer: 'bread'}`))
+
+	for i := range 10 {
+		time.Sleep(time.Second)
+		sse.MergeFragments(
+			fmt.Sprintf(`<div id="question">%d ? %d ?</div>`,
+				i,
+				time.Now().Unix()))
+		sse.MergeSignals([]byte(
+			fmt.Sprintf(`{title: '%d - %d'}`, i,
+				time.Now().Unix())))
+	}
 }
--- cmd/wmcserver/main.go
+++ cmd/wmcserver/main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	http.HandleFunc("/datastar.js", datastar.SourceHandler)
 	http.HandleFunc("/datastar.js.map", datastar.SourceMapHandler)
+	http.HandleFunc("/events.html", EventHandler)
 	http.HandleFunc("/index.html", IndexHandler)
 	http.HandleFunc("/", IndexHandler)
 	http.ListenAndServe("localhost:8081", nil)
